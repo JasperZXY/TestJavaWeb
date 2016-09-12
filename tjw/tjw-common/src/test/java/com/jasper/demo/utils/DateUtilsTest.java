8c57684b8c3cf96e52d2cbc667680630ab950794
JasperZXY <Jasperzxy@gamil.com>
2016-09-12 20:45:04 +0800

    时间工具类

---
+++ tjw/tjw-common/src/test/java/com/jasper/demo/utils/DateUtilsTest.java
@@ -0,0 +1,22 @@
+package com.jasper.demo.utils;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * @author Jasper.Zhong
+ */
+public class DateUtilsTest {
+    @Test
+    public void dateToString() {
+        System.out.println(DateUtils.dateToString(new Date()));
+        System.out.println(DateUtils.dateToString(new Date(), DateUtils.DATE_COMPACT_FORMAT));
+    }
+
+    @Test
+    public void dateStrToObject() {
+        System.out.println(DateUtils.dateStrToObject("2016-12-30 20:01:02"));
+        System.out.println(DateUtils.dateStrToObject("2016-12-30 20:01:02", DateUtils.DATE_TIME_FORMAT));
+    }
+}
